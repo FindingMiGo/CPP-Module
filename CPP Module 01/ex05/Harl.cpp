8990219a91477d9ef71b082b3d3e06f87daf48cc
astalow <endymion.0710@gmail.com>
2022-08-17 17:06:20 +0900

    01-05 done

---
+++ CPP Module 01/ex05/Harl.cpp
@@ -0,0 +1,36 @@
+#include "Harl.hpp"
+
+Harl::Harl(){
+    _func[0] = &Harl::debug;
+    _func[1] = &Harl::info;
+    _func[2] = &Harl::warning;
+    _func[3] = &Harl::error;
+}
+
+Harl::~Harl() {}
+
+void Harl::debug(){
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(){
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(){
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(){
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level){
+	int	index =	(level == "debug")		|
+				(level == "info") * 2	|
+				(level == "warning") * 3|
+				(level == "error") * 4;
+	if (index > 4 || index < 1)
+		return ;
+	(this->*_func[index - 1])();
+}
---
+++ CPP Module 01/ex05/Harl.hpp
@@ -0,0 +1,20 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+#include <iostream>
+
+class Harl{
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+	private:
+		void debug();
+		void info();
+		void warning();
+		void error();
+		void (Harl::*_func[4])();
+};
+
+#endif
