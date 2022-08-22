fedc8507c569eb6720e2284dd3cd564d3c04f8fc
astalow <endymion.0710@gmail.com>
2022-08-22 20:35:01 +0900

    03-00 done

---
+++ CPP Module 03/ex00/ClapTrap.cpp
@@ -0,0 +1,90 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(){
+    _name = "JohnDoe";
+    _hitPoints = 10;
+    _energyPoints = 10;
+    _attackDamage = 0;
+	std::cout << "Constructor called ("<< _name << ")"<< std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name){
+    _name = name;
+    _hitPoints = 10;
+    _energyPoints = 10;
+    _attackDamage = 0;
+    std::cout << "Constructor called ("<< _name << ")"<< std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &ct){
+	*this = ct;
+	std::cout << "Copy Constructor called ("<< _name << ")" << std::endl;
+}
+
+ClapTrap::~ClapTrap(){
+	std::cout << "Destructor called ("<< _name << ")" << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &ct){
+	if (this != &ct)	{
+		_name = ct._name;
+		_hitPoints = ct._hitPoints;
+		_energyPoints = ct._energyPoints;
+		_attackDamage = ct._attackDamage;
+	}
+	std::cout << "Assignment operator called ("<< _name << ")" << std::endl;
+	return *this;
+}
+
+void ClapTrap::attack(std::string const &target)
+{
+	if (_energyPoints == 0){
+		std::cout << "no energypoints" << std::endl;
+		return ;
+	}
+	if (_hitPoints == 0){
+		std::cout << "ClapTrap " << _name << " is no reply. It's just corpse." << std::endl;
+		return ;
+	}
+	std::cout 
+	<< "ClapTrap " << _name << " attacks " << target 
+	<< ", causing " << _attackDamage << " points of damage!" 
+	<< std::endl;
+	_energyPoints--;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitPoints == 0)
+		std::cout << "ClapTrap " << _name << " is no reply. It's just corpse." << std::endl;
+	else if (_hitPoints <= amount)
+	{
+		std::cout << "ClapTrap " << _name << " takes " << amount << " damage!" << std::endl;
+		std::cout << "ClapTrap " << _name << " is died." << std::endl;
+		_hitPoints = 0;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << _name << " takes " << amount << " damage!" << std::endl;
+		_hitPoints -= amount;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount){
+	if (_energyPoints == 0){
+		std::cout << "no energypoints" << std::endl;
+		return ;
+	}
+    if(_hitPoints == 0)
+        std::cout << "ClapTrap " << _name << " is no reply. It's just corpse." << std::endl;
+    else
+    {
+        std::cout <<"ClapTrap "<< _name << " recovered " << amount << "points!" << std::endl;
+        _hitPoints += amount;
+    }
+	_energyPoints--;
+}
+
+unsigned int  ClapTrap::getAttackDamege(){
+    return(_attackDamage);
+}
---
+++ CPP Module 03/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "ClapTrap.hpp"
+
+int main( void ) {
+    ClapTrap a;
+    ClapTrap b("b");
+    ClapTrap c(b);
+    
+    std::cout << std::endl;
+    a.attack("b");
+    b.takeDamage(a.getAttackDamege());
+    std::cout <<std::endl;
+    b.beRepaired(3);
+    b.takeDamage(10);
+    b.takeDamage(3);
+    std::cout <<std::endl;
+    b.attack("a");
+    b.beRepaired(3);
+    b.takeDamage(1);
+    std::cout << std::endl;
+}
