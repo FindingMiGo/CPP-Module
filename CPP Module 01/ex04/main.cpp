bd84095c9c0f0d915a02a747f066f0e247f47195
astalow <endymion.0710@gmail.com>
2022-08-29 00:33:40 +0900

    add error message

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -1,8 +1,10 @@
 #include "Sed.hpp"
 
 int main(int ac, char **av){
-	if (ac != 4)
+	if (ac != 4){
+		std::cerr << "Invalid args" << std::endl;
 		return (0);
+	}
 	if (av[2][0] == '\0' || av[3][0] == '\0')
 		return (0);
 	Sed sed(av[1], av[2], av[3]);
