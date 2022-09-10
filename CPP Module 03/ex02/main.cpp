793f98bafdfb6efb11ecc26af63865851c2d7797
astalow <endymion.0710@gmail.com>
2022-09-11 00:17:18 +0900

    03-02 add test

--- CPP Module 03/ex02/main.cpp
+++ CPP Module 03/ex02/main.cpp
@@ -1,7 +1,28 @@
-#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
-int main(void)
-{
-	return (0);
+int main(){
+	std::cout << "===constructor test===\n\n";
+	{
+		FragTrap a;
+		FragTrap b(a);
+		FragTrap c("c");
+	}
+	std::cout << "\n===attack and takeDamage test===\n\n";
+	{
+		FragTrap a("Brave");
+		FragTrap b("Devil");
+		std::cout << std::endl;
+		a.attack("Devil");
+		b.takeDamage(a.getAttackDamege());
+		b.attack("Brave");
+		a.takeDamage(100);
+		a.attack("Devil");
+		a.beRepaired(10);
+	}
+	std::cout << "\n===highFivesGuys test===\n\n";
+	{
+		FragTrap a("Brave");
+		a.highFivesGuys();
+	}
 }
