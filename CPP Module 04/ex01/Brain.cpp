ea43e76aec36875f0afb31a347fd2187545f5880
astalow <endymion.0710@gmail.com>
2022-09-29 19:51:40 +0900

    04-01 メッセージの書式を修正

--- CPP Module 04/ex01/Brain.cpp
+++ CPP Module 04/ex01/Brain.cpp
@@ -1,18 +1,18 @@
 #include "Brain.hpp"
 
 Brain::Brain(){
-	std::cout << "Constuctor called (Brain)" << std::endl;
+	std::cout << "Brain constuctor called" << std::endl;
 	for (int i = 0; i < 100; i++)
 		_ideas[i] = 'B';
 }
 
 Brain::Brain(const Brain &b){
-	std::cout << "Copy Constuctor called (Brain)" << std::endl;
+	std::cout << "Brain copy Constuctor called" << std::endl;
 	*this = b;
 }
 
 Brain::~Brain(){
-	std::cout << "Destuctor called (Brain)" << std::endl;
+	std::cout << "Brain destuctor called" << std::endl;
 }
 
 Brain &Brain::operator=(const Brain &b){
