22d87ceeae91a8d9da4acfea1508c0550c774e1e
astalow <endymion.0710@gmail.com>
2022-11-14 04:04:21 +0900

    テスト追加

--- CPP Module 08/ex02/main.cpp
+++ CPP Module 08/ex02/main.cpp
@@ -1,6 +1,6 @@
 #include "MutantStack.hpp"
 
-int main() {
+void test1() {
   MutantStack<int> mstack;
   mstack.push(5);
   mstack.push(17);
@@ -21,5 +21,51 @@ int main() {
     ++it;
   }
   std::stack<int> s(mstack);
+}
+
+void test1_const() {
+  MutantStack<int> mstack;
+  mstack.push(5);
+  mstack.push(17);
+  std::cout << mstack.top() << std::endl;
+  mstack.pop();
+  std::cout << mstack.size() << std::endl;
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
+  mstack.push(0);
+  MutantStack<int>::const_iterator it = mstack.begin();
+  MutantStack<int>::const_iterator ite = mstack.end();
+  ++it;
+  --it;
+  while (it != ite) {
+    std::cout << *it << std::endl;
+    ++it;
+  }
+  std::stack<int> s(mstack);
+}
+
+void test2() {
+  MutantStack<int> mstack;
+  std::list<int> lst;
+
+  for (int i = 0; i < 10; i++) {
+    mstack.push(i);
+    lst.push_back(i);
+  }
+  MutantStack<int>::iterator mitr = mstack.begin();
+  std::list<int>::iterator litr = lst.begin();
+  for (int i = 0; i < 10; i++) {
+    std::cout << "mstack: " << *mitr << "\tlist: " << *litr << std::endl;
+  }
+}
+
+int main() {
+  std::cout << "subject test\n";
+  test1();
+  std::cout << "\nsubject test(const)\n";
+  test1_const();
+  std::cout << "\ntest to compare with stacks and lists\n";
+  test2();
   return 0;
 }
